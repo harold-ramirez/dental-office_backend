b9d021ced4b24c117bbcc3c4d660ee2bbba5922d
harold-ramirez <ramirezpereiraharold@gmail.com>
2026-01-25 23:46:56 -0400

    Add Prisma config and update Prisma dependencies
    Added prisma.config.ts for Prisma configuration. Upgraded @prisma/client and prisma to v7, added @prisma/adapter-mariadb, and updated related dependencies. Adjusted .gitignore to exclude /generated/prisma. Updated PrismaService to align with new Prisma version.

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,8 +1,19 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from './generated/prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const adapter = new PrismaMariaDb({
+      host: 'localhost',
+      user: 'root',
+      password: '1717',
+      database: 'dbdentaloffice',
+    });
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
